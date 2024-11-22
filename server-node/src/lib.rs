2dd4a2879a7c713416a5d50594384a0489104ac2
Lauri Gustafsson <lauri.gustafsson@iki.fi>
2024-11-22 18:14:53 +0200

    server-node/: Refactor tests to be independent of connection order

--- server-node/src/lib.rs
+++ server-node/src/lib.rs
@@ -199,6 +199,11 @@ mod tests {
         any::Any, cell::RefCell, collections::VecDeque, iter, net::SocketAddr, sync::MutexGuard,
     };
 
+    struct MessageSequence {
+        peer_addr: SocketAddr,
+        messages: Vec<Message>,
+    }
+
     struct MockStreamRaw {
         connected: Option<SocketAddr>,
         read_timeout: RefCell<Option<Duration>>,
@@ -215,8 +220,8 @@ mod tests {
             self.inner.lock().unwrap()
         }
 
-        fn send_replies(&mut self, sequence: &[Message]) {
-            for message in sequence {
+        fn send_replies(&mut self, sequence: &MessageSequence) {
+            for message in &sequence.messages {
                 ciborium::into_writer::<Message, &mut VecDeque<u8>>(
                     message,
                     &mut self.inner().reply_data,
@@ -249,14 +254,34 @@ mod tests {
 
     impl MessageStream for MockStream {
         fn connect(addr: impl ToSocketAddrs) -> std::io::Result<Self> {
-            Ok(Self {
-                inner: Arc::new(Mutex::new(MockStreamRaw {
-                    connected: addr.to_socket_addrs()?.next(),
-                    read_timeout: RefCell::new(None),
-                    sent_data: VecDeque::new(),
-                    reply_data: VecDeque::new(),
-                })),
-            })
+            // Parse the address
+            let addr = addr.to_socket_addrs()?.next();
+
+            // Check if a message sequence has been defined for the peer.
+            // Construct a MockStream if defined, return ConnectionRefused, if not.
+            // TODO for an even fancier solution, we should develop a mechanism
+            // to support multiple message sequences per peer address, i.e.
+            // if connect() is called repeatedly.
+            with_replies(
+                |replies| match replies.iter().find(|seq| Some(seq.peer_addr) == addr) {
+                    Some(replies) => {
+                        let mut stream = Self {
+                            inner: Arc::new(Mutex::new(MockStreamRaw {
+                                connected: addr,
+                                read_timeout: RefCell::new(None),
+                                sent_data: VecDeque::new(),
+                                reply_data: VecDeque::new(),
+                            })),
+                        };
+                        stream.send_replies(replies);
+                        Ok(stream)
+                    }
+                    None => Err(std::io::Error::new(
+                        std::io::ErrorKind::ConnectionRefused,
+                        "Connection refused by peer",
+                    )),
+                },
+            )
         }
 
         fn set_read_timeout(&self, dur: Option<Duration>) -> std::io::Result<()> {
@@ -277,41 +302,40 @@ mod tests {
 
         // In tests where the MockStreams should reply to the server,
         // you must push the desired message sequences to this global.
-        // See [`push_reply_sequence`] for pushing message sequences.
-        // Outer vec stores message sequences in the order of established connections,
-        // Inner vec is the sequence of Messages
-        static REPLIES: RefCell<Vec<Vec<Message>>> = const {RefCell::new(Vec::new())};
+        // See [`push_replies`] for setting up message sequences.
+        static REPLIES: RefCell<Vec<MessageSequence>> = const {RefCell::new(Vec::new())};
     }
 
-    fn with_streams(f: impl FnOnce(&mut Vec<MockStream>)) {
+    fn with_stream_of_peer<T>(key: SocketAddr, f: impl FnOnce(&MockStream) -> T) -> T {
+        with_streams(|streams| -> T {
+            let stream = streams
+                .iter()
+                .find(|stream| stream.inner().connected == Some(key))
+                .expect("Tried to inspect stream with undefined peer in test");
+            f(stream)
+        })
+    }
+
+    fn with_streams<T>(f: impl FnOnce(&mut Vec<MockStream>) -> T) -> T {
         STREAMS.with_borrow_mut(f)
     }
 
-    fn with_replies(f: impl FnOnce(&mut Vec<Vec<Message>>)) {
+    fn with_replies<T>(f: impl FnOnce(&mut Vec<MessageSequence>) -> T) -> T {
         REPLIES.with_borrow_mut(f)
     }
 
     pub fn push_stream(stream: impl MessageStream + 'static) {
         with_streams(|streams| {
-            with_replies(|replies| {
-                let stream_index = streams.len();
-
-                let stream: Box<dyn Any> = Box::new(stream.clone());
-                let mut stream = *(stream
-                    .downcast::<MockStream>()
-                    .expect("Non-mock MessageStream used in tests"));
-
-                if let Some(sequence) = replies.get(stream_index) {
-                    stream.send_replies(sequence);
-                }
-
-                streams.push(stream);
-            });
+            let stream: Box<dyn Any> = Box::new(stream.clone());
+            let stream = *(stream
+                .downcast::<MockStream>()
+                .expect("Non-mock MessageStream used in tests"));
+            streams.push(stream);
         })
     }
 
-    fn push_reply_sequence(sequence: Vec<Message>) {
-        with_replies(|replies| replies.push(sequence));
+    fn push_replies(sequences: impl IntoIterator<Item = MessageSequence>) {
+        with_replies(|replies| replies.extend(sequences));
     }
 
     #[test]
@@ -331,11 +355,16 @@ mod tests {
 
     #[test]
     fn cluster_connect_to_one_peer() {
-        push_reply_sequence(vec![Message::JoinAck(1)]);
+        let peer_1_addr = "123.123.123.123:8909".parse().unwrap();
+
+        push_replies(vec![MessageSequence {
+            peer_addr: peer_1_addr,
+            messages: vec![Message::JoinAck(1)],
+        }]);
 
         let config = Config {
             address_private: ([0u8; 4], 1234).into(),
-            peers: vec!["123.123.123.123:8909".parse().unwrap()],
+            peers: vec![peer_1_addr],
             id: 0,
         };
         let cluster = Cluster::connect::<MockStream>(config);
@@ -347,24 +376,29 @@ mod tests {
             assert_eq!(streams[0].get_messages(), vec![Message::Join(0)]);
             assert_eq!(streams.len(), 1);
             let stream = streams[0].inner();
-            assert_eq!(
-                stream.connected,
-                Some("123.123.123.123:8909".parse().unwrap())
-            );
+            assert_eq!(stream.connected, Some(peer_1_addr));
         });
     }
 
     #[test]
     fn cluster_connect_to_two_peers() {
-        push_reply_sequence(vec![Message::JoinAck(1)]);
-        push_reply_sequence(vec![Message::JoinAck(2)]);
+        let peer_1_addr = "123.123.123.123:8909".parse().unwrap();
+        let peer_2_addr = "1.1.1.1:22".parse().unwrap();
+
+        push_replies(vec![
+            MessageSequence {
+                peer_addr: peer_1_addr,
+                messages: vec![Message::JoinAck(1)],
+            },
+            MessageSequence {
+                peer_addr: peer_2_addr,
+                messages: vec![Message::JoinAck(2)],
+            },
+        ]);
 
         let config = Config {
             address_private: ([0u8; 4], 1234).into(),
-            peers: vec!["123.123.123.123:8909", "1.1.1.1:22"]
-                .into_iter()
-                .map(|s| s.parse().unwrap())
-                .collect(),
+            peers: vec![peer_1_addr, peer_2_addr],
             id: 0,
         };
         let cluster = Cluster::connect::<MockStream>(config);
@@ -373,17 +407,34 @@ mod tests {
         assert!(cluster.server.leases.lock().unwrap().is_empty());
         assert_eq!(cluster.server.id, 0);
         with_streams(|streams| {
-            assert_eq!(streams[0].get_messages(), vec![Message::Join(0)]);
-            assert_eq!(streams[1].get_messages(), vec![Message::Join(0)]);
             assert_eq!(streams.len(), 2);
-            assert_eq!(
-                streams[0].inner().connected,
-                Some("123.123.123.123:8909".parse().unwrap())
-            );
-            assert_eq!(
-                streams[1].inner().connected,
-                Some("1.1.1.1:22".parse().unwrap())
-            );
         });
+        with_stream_of_peer(peer_1_addr, |stream| {
+            assert_eq!(stream.get_messages(), vec![Message::Join(0)]);
+            assert_eq!(stream.inner().connected, Some(peer_1_addr));
+        });
+        with_stream_of_peer(peer_2_addr, |stream| {
+            assert_eq!(stream.get_messages(), vec![Message::Join(0)]);
+            assert_eq!(stream.inner().connected, Some(peer_2_addr));
+        });
+    }
+
+    // TODO refactor repeated test logic to function
+
+    #[test]
+    fn cluster_connect_to_offline_peer() {
+        let peer_1_addr = "123.123.123.123:8909".parse().unwrap();
+
+        // push_replies omitted
+
+        let config = Config {
+            address_private: ([0u8; 4], 1234).into(),
+            peers: vec![peer_1_addr],
+            id: 0,
+        };
+        let cluster = Cluster::connect::<MockStream>(config);
+
+        assert!(cluster.peers.is_empty());
+        with_streams(|streams| assert!(streams.is_empty()));
     }
 }

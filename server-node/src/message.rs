c0a2454b7c19941f54af4c619f4f7c1d09e87679
Lauri Gustafsson <lauri.gustafsson@iki.fi>
2024-11-29 16:45:38 +0200

    server/message.rs: Fix recv_timeout and add comments

--- server-node/src/message.rs
+++ server-node/src/message.rs
@@ -27,20 +27,29 @@ pub enum Message {
     Update(Lease),
 }
 
+/// # Read a message from a [`TcpStream`].
+/// This function can block the calling thread for the stream's current timeout setting (see [`TcpStream::set_read_timeout`]).
 pub fn recv(stream: &TcpStream) -> Result<Message, ciborium::de::Error<io::Error>> {
     ciborium::from_reader(stream)
 }
 
+/// # Read a message from a [`TcpStream`], with a timeout.
+/// This function can block the calling thread for the specified timeout duration.
+/// ## Concurrency
+/// This function may not be used concurrently with a stream that has been shared between different threads.
+/// Doing so may result in unexpected changes to the stream's timeout.
 pub fn recv_timeout(
     stream: &TcpStream,
     timeout: Duration,
 ) -> Result<Message, ciborium::de::Error<io::Error>> {
+    let previous_timeout = stream.read_timeout().unwrap();
     stream.set_read_timeout(Some(timeout)).unwrap();
     let result = recv(stream);
-    stream.set_read_timeout(None).unwrap();
+    stream.set_read_timeout(previous_timeout).unwrap();
     result
 }
 
+/// # Send a message over a [`TcpStream`]
 pub fn send(stream: &TcpStream, message: &Message) -> Result<(), ciborium::ser::Error<io::Error>> {
     ciborium::into_writer(message, stream)
 }

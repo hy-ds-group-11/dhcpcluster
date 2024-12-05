77cad32e02e58a5e170843a9b7a0fda1dbe3ad50
Simo Soini <simo@capscience.fi>
2024-12-05 13:21:23 +0200

    dhcp-message: Remove redundant wrapper struct.

--- dhcp-message/src/lib.rs
+++ dhcp-message/src/lib.rs
@@ -6,7 +6,7 @@ use serde::{Deserialize, Serialize};
 use std::{io::Read, net::Ipv4Addr};
 
 #[derive(Serialize, Deserialize, Debug)]
-pub enum DHCPRequest {
+pub enum DhcpMessage {
     Discover {
         mac_address: Vec<u8>,
     },
@@ -23,26 +23,15 @@ pub enum DHCPRequest {
     Nak,
 }
 
-#[derive(Debug)]
-pub struct DhcpMessage {
-    pub request: DHCPRequest,
-}
-
 impl DhcpMessage {
-    pub fn new(request: DHCPRequest) -> Self {
-        Self { request }
-    }
-
     pub fn encode(&self) -> Vec<u8> {
         let mut data = Vec::new();
-        ser::into_writer(&self.request, &mut data).unwrap();
+        ser::into_writer(&self, &mut data).unwrap();
 
         data
     }
 
     pub fn decode(bytes: impl Read) -> Self {
-        let request = de::from_reader(bytes).unwrap();
-
-        Self { request }
+        de::from_reader(bytes).unwrap()
     }
 }

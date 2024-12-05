72b96c55cbdffc6b1a158e5342d595cea746fd5d
Simo Soini <simo@capscience.fi>
2024-12-05 13:42:48 +0200

    dhcp-message: Separate client and server specific message types.

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -1,17 +1,17 @@
 use std::net::Ipv4Addr;
 
-use dhcp_message::DhcpMessage;
+use dhcp_message::{DhcpClientMessage, DhcpServerMessage};
 
-pub fn send_discover(mac_address: Vec<u8>) -> DhcpMessage {
-    let message = DhcpMessage::Discover { mac_address };
+pub fn get_offer(mac_address: Vec<u8>) -> Option<DhcpServerMessage> {
+    let message = DhcpClientMessage::Discover { mac_address };
     let _data = message.encode();
 
     // TODO: Send message and get response
     unimplemented!()
 }
 
-pub fn send_request(mac_address: Vec<u8>, ip: Ipv4Addr) -> DhcpMessage {
-    let message = DhcpMessage::Request { mac_address, ip };
+pub fn get_ack(mac_address: Vec<u8>, ip: Ipv4Addr) -> Option<DhcpServerMessage> {
+    let message = DhcpClientMessage::Request { mac_address, ip };
     let _data = message.encode();
 
     // TODO: Send message and get response
--- dhcp-message/src/lib.rs
+++ dhcp-message/src/lib.rs
@@ -6,24 +6,36 @@ use serde::{Deserialize, Serialize};
 use std::{io::Read, net::Ipv4Addr};
 
 #[derive(Serialize, Deserialize, Debug)]
-pub enum DhcpMessage {
-    Discover {
-        mac_address: Vec<u8>,
-    },
+pub enum DhcpClientMessage {
+    Discover { mac_address: Vec<u8> },
+    Request { mac_address: Vec<u8>, ip: Ipv4Addr },
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum DhcpServerMessage {
     Offer {
         ip: Ipv4Addr,
         lease_time: u32,
         subnet_mask: Ipv4Addr,
     },
-    Request {
-        mac_address: Vec<u8>,
-        ip: Ipv4Addr,
-    },
     Ack,
     Nak,
 }
 
-impl DhcpMessage {
+impl DhcpClientMessage {
+    pub fn encode(&self) -> Vec<u8> {
+        let mut data = Vec::new();
+        ser::into_writer(&self, &mut data).unwrap();
+
+        data
+    }
+
+    pub fn decode(bytes: impl Read) -> Self {
+        de::from_reader(bytes).unwrap()
+    }
+}
+
+impl DhcpServerMessage {
     pub fn encode(&self) -> Vec<u8> {
         let mut data = Vec::new();
         ser::into_writer(&self, &mut data).unwrap();

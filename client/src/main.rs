849659f08caf93395a0703f4a7668f29aebd63e9
Simo Soini <simo@capscience.fi>
2024-12-11 16:50:43 +0200

    client: Add commands to test renewing leases.

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,17 +1,20 @@
 use client::{config::Config, CommunicationError};
-use protocol::{DhcpOffer, MacAddr};
+use protocol::{DhcpOffer, MacAddr, MacAddrParseError};
 use rand::Rng;
 use rustyline::{error::ReadlineError, DefaultEditor};
 use std::{
     error::Error,
     fmt::Display,
     io,
-    net::{SocketAddr, TcpStream, ToSocketAddrs},
+    iter::Peekable,
+    net::{AddrParseError, Ipv4Addr, SocketAddr, TcpStream, ToSocketAddrs},
     num::ParseIntError,
     ops::Range,
     path::{Path, PathBuf},
     process::exit,
+    str::FromStr,
     time::{Duration, Instant},
+    vec::IntoIter,
 };
 use thiserror::Error;
 
@@ -24,21 +27,24 @@ enum QueryTarget<'a> {
     Arbitrary(&'a str),
 }
 
-enum QueryMode {
-    Discover,
-    Renew,
-}
-
 struct Query<'a> {
     batch: QueryCount,
-    mode: QueryMode,
     target: QueryTarget<'a>,
     verbose: bool,
+    mac_address: Option<MacAddr>,
+}
+
+struct Renew<'a> {
+    target: QueryTarget<'a>,
+    mac_address: MacAddr,
+    ip_address: Ipv4Addr,
 }
 
 enum Command<'a> {
     Nop,
     Query(Query<'a>),
+    Renew(Renew<'a>),
+    GenerateMac,
     List,
     Help,
     Conf,
@@ -65,6 +71,10 @@ Supported commands:
             random  pick randomly from configured servers (default)
             <index> index number of server to connect to (see list)
             <host>  arbitrary host, e.g. dhcp.example.com:4321
+    \x1b[32mrenew\x1b[0m <\x1b[36mMAC_ADDRESS\x1b[0m> <\x1b[36mIP_ADDRESS\x1b[0m> [\x1b[36mSERVER\x1b[0m]
+        Renew an existing lease with MAC_ADDRESS and IP_ADDRESS from SERVER
+    \x1b[32mgenerate\x1b[0m
+        Generate a random MAC-address
     \x1b[32mlist\x1b[0m
         List configured servers
     \x1b[32mconf\x1b[0m
@@ -74,7 +84,7 @@ Supported commands:
     \x1b[32mquit\x1b[0m
         Exit the CLI
 
-Supported shorthands: qr, ls, cf, h, q
+Supported shorthands: qr, re, gen, ls, cf, h, q
 "
     );
 }
@@ -87,13 +97,17 @@ enum CommandParseError<'a> {
     UnrecognizedArguments(Vec<&'a str>),
     #[error("Unrecognized command: {0:?}")]
     UnrecognizedCommand(&'a str),
+    #[error("Invalid MAC address in command arguments")]
+    InvalidMacAddress(#[from] MacAddrParseError),
+    #[error("Invalid IP address in command arguments")]
+    InvalidIpAddress(#[from] AddrParseError),
 }
 
 impl<'a> Query<'a> {
     fn parse(mut args: &[&'a str]) -> Result<Self, CommandParseError<'a>> {
         let mut batch = 1;
-        let mut mode = QueryMode::Discover;
         let mut verbose = false;
+        let mut mac_address = None;
 
         loop {
             match args {
@@ -101,12 +115,12 @@ impl<'a> Query<'a> {
                     batch = count.parse()?;
                     args = rest;
                 }
-                ["-r", rest @ ..] => {
-                    mode = QueryMode::Renew;
-                    args = rest;
-                }
                 ["-v", rest @ ..] => {
                     verbose = true;
+                    args = rest;
+                }
+                ["-m", address, rest @ ..] => {
+                    mac_address = Some(MacAddr::from_str(address)?);
                     args = rest;
                 }
                 [..] => break,
@@ -124,10 +138,33 @@ impl<'a> Query<'a> {
 
         Ok(Query {
             batch,
-            mode,
             target,
             verbose,
+            mac_address,
         })
+    }
+}
+
+impl<'a> Renew<'a> {
+    fn parse(args: &[&'a str]) -> Result<Self, CommandParseError<'a>> {
+        match args {
+            [mac_address, ip_address, rest @ ..] => {
+                let target = match rest {
+                    [] | ["random"] => QueryTarget::Random,
+                    [target] => match target.parse() {
+                        Ok(index) => QueryTarget::Specific(index),
+                        Err(_) => QueryTarget::Arbitrary(target),
+                    },
+                    rest => Err(CommandParseError::UnrecognizedArguments(rest.into()))?,
+                };
+                Ok(Renew {
+                    target,
+                    mac_address: mac_address.parse()?,
+                    ip_address: ip_address.parse()?,
+                })
+            }
+            args => Err(CommandParseError::UnrecognizedArguments(args.into()))?,
+        }
     }
 }
 
@@ -136,6 +173,8 @@ fn parse_command(line: &str) -> Result<Command, CommandParseError> {
     match command.as_slice() {
         ["exit" | "quit" | "q"] => Ok(Command::Quit),
         ["query" | "qr", args @ ..] => Ok(Command::Query(Query::parse(args)?)),
+        ["renew" | "re", args @ ..] => Ok(Command::Renew(Renew::parse(args)?)),
+        ["generate" | "gen"] => Ok(Command::GenerateMac),
         ["list" | "ls"] => Ok(Command::List),
         ["conf" | "cf"] => Ok(Command::Conf),
         ["help" | "h"] => Ok(Command::Help),
@@ -224,36 +263,21 @@ fn query(
     server: &str,
     default_port: u16,
     timeout: Option<Duration>,
+    mac_address: MacAddr,
 ) -> Result<QuerySuccess, QueryError> {
     let start = Instant::now();
 
-    // Resolve server name, try server itself first
-    let mut addrs = match server.to_socket_addrs() {
-        Ok(addrs) => addrs,
-        Err(e) => {
-            // If the server name did not contain port number, try with default port number
-            if e.kind() == std::io::ErrorKind::InvalidInput {
-                (server, default_port)
-                    .to_socket_addrs()
-                    .map_err(|e| QueryError::NameResolution { server, source: e })?
-            } else {
-                return Err(QueryError::NameResolution { server, source: e });
-            }
-        }
-    }
-    .peekable();
-
-    let mac_addr = random_mac_addr();
+    let mut addrs = get_addresses(server, default_port)?;
 
     while let Some(addr) = addrs.next() {
         let stream = connect_timeout(&addr, timeout, server)?;
-        match client::get_offer(&stream, mac_addr) {
+        match client::get_offer(&stream, mac_address) {
             Err(e) => match addrs.peek() {
                 Some(_) => continue, // Try next DNS result
                 None => return Err(QueryError::Communication { server, source: e }),
             },
             Ok(Some(offer @ DhcpOffer { ip, .. })) => {
-                if client::get_ack(&stream, mac_addr, ip)
+                if client::get_ack(&stream, mac_address, ip)
                     .map_err(|e| QueryError::Communication { server, source: e })?
                 {
                     return Ok(QuerySuccess {
@@ -269,6 +293,68 @@ fn query(
         }
     }
     Err(QueryError::UnreachableServer { server })
+}
+
+fn renew(
+    server: &str,
+    default_port: u16,
+    timeout: Option<Duration>,
+    mac_address: MacAddr,
+    ip_address: Ipv4Addr,
+) -> Result<QuerySuccess, QueryError> {
+    let start = Instant::now();
+
+    let mut addrs = get_addresses(server, default_port)?;
+
+    while let Some(addr) = addrs.next() {
+        let stream = connect_timeout(&addr, timeout, server)?;
+        match client::get_ack(&stream, mac_address, ip_address) {
+            Err(e) => match addrs.peek() {
+                Some(_) => continue, // Try next DNS result
+                None => return Err(QueryError::Communication { server, source: e }),
+            },
+            Ok(acked) =>
+            // TODO: offer TTL and subnet mask are incorrect;
+            {
+                if acked {
+                    return Ok(QuerySuccess {
+                        offer: DhcpOffer {
+                            ip: ip_address,
+                            lease_time: 3600,
+                            subnet_mask: 24,
+                        },
+                        server,
+                        time: start.elapsed(),
+                    });
+                } else {
+                    return Err(QueryError::RequestNack { server });
+                }
+            }
+        }
+    }
+    Err(QueryError::UnreachableServer { server })
+}
+
+/// Resolve server name, try server itself first
+fn get_addresses(
+    server: &str,
+    default_port: u16,
+) -> Result<Peekable<IntoIter<SocketAddr>>, QueryError> {
+    let addrs = match server.to_socket_addrs() {
+        Ok(addrs) => addrs,
+        Err(e) => {
+            // If the server name did not contain port number, try with default port number
+            if e.kind() == std::io::ErrorKind::InvalidInput {
+                (server, default_port)
+                    .to_socket_addrs()
+                    .map_err(|e| QueryError::NameResolution { server, source: e })?
+            } else {
+                return Err(QueryError::NameResolution { server, source: e });
+            }
+        }
+    }
+    .peekable();
+    Ok(addrs)
 }
 
 #[derive(Error, Debug)]
@@ -290,10 +376,13 @@ fn handle_query_command(cmd: Query, config: &Config) -> Result<(), QueryExecutio
 
     for _ in 0..cmd.batch {
         let server = server.unwrap_or_else(|| random_server(config));
-        let res = match cmd.mode {
-            QueryMode::Discover => query(server, config.default_port, config.timeout),
-            QueryMode::Renew => todo!(),
+        let mac_address = if let Some(mac_address) = cmd.mac_address {
+            mac_address
+        } else {
+            random_mac_addr()
         };
+
+        let res = query(server, config.default_port, config.timeout, mac_address);
         results.push(res);
     }
 
@@ -344,6 +433,31 @@ fn handle_query_command(cmd: Query, config: &Config) -> Result<(), QueryExecutio
     Ok(())
 }
 
+fn handle_renew_command(cmd: Renew, config: &Config) -> Result<(), QueryExecutionError> {
+    let server = match cmd.target {
+        QueryTarget::Random => random_server(config),
+        QueryTarget::Specific(i) => config.servers.get(i).map(|s| s.as_str()).ok_or(
+            QueryExecutionError::InvalidServerIndex(i, 0..config.servers.len()),
+        )?,
+        QueryTarget::Arbitrary(host) => host,
+    };
+
+    let res = renew(
+        server,
+        config.default_port,
+        config.timeout,
+        cmd.mac_address,
+        cmd.ip_address,
+    );
+
+    match res {
+        Ok(success) => println!("{success}"),
+        Err(e) => print_error(e),
+    }
+
+    Ok(())
+}
+
 fn main() -> Result<(), ReadlineError> {
     let config_file_path: PathBuf = std::env::args_os()
         .nth(1)
@@ -381,6 +495,10 @@ fn main() -> Result<(), ReadlineError> {
                     Ok(Query(query)) => {
                         handle_query_command(query, &config).unwrap_or_else(print_error)
                     }
+                    Ok(Renew(renew)) => {
+                        handle_renew_command(renew, &config).unwrap_or_else(print_error)
+                    }
+                    Ok(GenerateMac) => println!("{}", random_mac_addr()),
                     Ok(List) => list(&config),
                     Ok(Conf) => println!("\x1b[32m{config:#?}\x1b[0m"),
                     Ok(Help) => help(),
